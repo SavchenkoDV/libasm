dd7603fd52812bb04e8daec04320e91bdb125e41
dvsavchenko <dvs.rocket@gmail.com>
2024-12-16 18:04:03 +0000

    ft_write done

--- ft_write.s
+++ ft_write.s
@@ -1,13 +1,18 @@
 global ft_write
+extern __errno_location
 
 ft_write:
     mov rax, 1
     syscall
-    cmp rax, -1
-    jne .error
-    ret 
+    cmp rax, 0
+    jl .error
+    ret
 .error:
-    
+    neg rax
+    mov rbx, rax
+    call __errno_location wrt ..plt
+    mov [rax], rbx
+    mov rax, -1
     ret
 
-section .note.GNU-stack noalloc noexec nowrite progbits    
+section .note.GNU-stack noalloc noexec nowrite progbits 
